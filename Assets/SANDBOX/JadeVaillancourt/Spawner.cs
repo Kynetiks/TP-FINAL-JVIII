ce580dc77042b875fc3796a95a9a4beaae07eb02
Jade Vaillancourt <jade.vaillancourt24@hotmail.com>
2024-11-26 17:43:04 -0500

    Script pour faire des waves

--- Assets/SANDBOX/JadeVaillancourt/Spawner.cs
+++ Assets/SANDBOX/JadeVaillancourt/Spawner.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-  
-  public GameObject[] objetsQuiSpawn;
+  /*PREMIER SPAWN
+ /* public GameObject[] objetsQuiSpawn;
 
     // Update is called once per frame
     void Update()
@@ -19,4 +19,45 @@ public class Spawner : MonoBehaviour
             Instantiate(objetsQuiSpawn[randomIndex], randomSpawnPosition, Quaternion.identity);
         }
     }
+
+    */
+
+    public float NombreSpawn = 1.0f;
+    public float tempsEntreWaves = 3.0f;
+
+    public int NombreEnnemi;
+
+    public GameObject Ennemi;
+
+    bool WaveOver = true;
+
+
+    void Update(){
+
+        if(WaveOver == true){
+
+            StartCoroutine(WaveSpawner());
+        }
+
+    }
+
+
+    IEnumerator WaveSpawner(){
+
+        WaveOver = false;
+
+        for(int i = 0; i < NombreEnnemi; i++){
+
+            GameObject EnnemiClone = Instantiate(Ennemi);
+
+            yield return new WaitForSeconds(NombreSpawn);
+        }
+
+        NombreSpawn -= 0.1f;
+        yield return new WaitForSeconds(tempsEntreWaves);
+
+        WaveOver = true;
+    }
+
+
 }
