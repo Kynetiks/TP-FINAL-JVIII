f1ff87eb6991035b691b636fbce132f652e357d7
Jade Vaillancourt <jade.vaillancourt24@hotmail.com>
2024-12-03 21:15:56 -0500

    Avancement Code pour Spawn

--- Assets/SANDBOX/JadeVaillancourt/Spawn final scripy/EnemyWaveData.cs
+++ Assets/SANDBOX/JadeVaillancourt/Spawn final scripy/EnemyWaveData.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 public class EnemyWaveData : ScriptableObject
 {
     public GameObject enemyPrefab; // Le prefab de l'ennemi à spawn
-    public int initialEnemyCount = 10; // Nombre d'ennemis dans la première vague
-    public float spawnInterval = 1.0f; // Temps entre chaque spawn d'ennemi
-    public float timeBetweenWaves = 3.0f; // Temps entre les vagues d'ennemis
-    public int enemiesPerWaveIncrease = 5; // Augmenter le nombre d'ennemis par vague
+    public int spawnAmount = 10; // Nombre d'ennemis par vague
+    public float spawnDelay = 0.5f; // Temps entre chaque spawn d'ennemi
+    public float BetweenWavesDelay = 3.0f; // Temps entre les vagues d'ennemis
+    public int enemiesPerWaveIncrease = 10; // Augmenter le nombre d'ennemis par vague
    
  
 }
