8318532d9faa08b4b2267080d9da306484eff447
iLOVEdisney2023 <mialgalong@hotmail.com>
2024-12-04 15:16:20 -0500

    fin UI main

--- Assets/SANDBOX/AliciaMichaud/RadialSelection.cs
+++ Assets/SANDBOX/AliciaMichaud/RadialSelection.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 public class RadialSelection : MonoBehaviour
-{   [Range(2,10)]
+{   
+    public OVRInput.Button spawnButton;
+    [Range(2,10)]
     public int numberOfRadialPart;
     public GameObject radialPartPrefab;
     public Transform radialPartCanvas;
     public float angleBetweenPart=10;
     public Transform handTransform;
 
+    public UnityEvent<int> OnPartSelected;
     private List<GameObject> spawnedParts= new List<GameObject>();
     private int currentSelectedRadialPart=-1;
 
@@ -23,10 +27,20 @@ public class RadialSelection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SpawnRadialPart();
-        GetSelectedRadialPart();
+        if(OVRInput.GetDown(spawnButton)){
+            SpawnRadialPart();
+        }
+        if(OVRInput.Get(spawnButton)){
+            GetSelectedRadialPart();
+        }
+        if(OVRInput.GetUp(spawnButton)){
+            HideAndTriggerSelected();
+        }
     }
-
+    public void HideAndTriggerSelected(){
+        OnPartSelected.Invoke(currentSelectedRadialPart);
+        radialPartCanvas.gameObject.SetActive(false);
+    }
     public void GetSelectedRadialPart(){
         Vector3 centerToHand= handTransform.position - radialPartCanvas.position;
         Vector3 centerToHandProjected= Vector3.ProjectOnPlane(centerToHand, radialPartCanvas.forward);
@@ -53,8 +67,11 @@ public class RadialSelection : MonoBehaviour
         }
     }
      public void SpawnRadialPart()
-    
     {
+        radialPartCanvas.gameObject.SetActive(true);
+        radialPartCanvas.position=handTransform.position;
+        radialPartCanvas.rotation= handTransform.rotation;
+
         foreach(var item in spawnedParts){
             Destroy(item);
         }
