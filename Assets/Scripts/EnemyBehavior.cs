193898aa8c707ce5bfdfc78e1f5be9114f8ee962
Kynetiks <lauriand.pro@gmail.com>
2024-12-15 20:32:16 -0500

    Animation Gobelin et Géant

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemyBehavior : MonoBehaviour
 {
     [SerializeField] GameObject vfxSpawn;
     [SerializeField] GameObject vfxDeath;
+    private Animator animator;  // Référence à l'Animator de l'ennemi
+    //private bool NearTarget = false;  // Indicateur si l'ennemi est à portée du cristal
+
     public void Spawn()
     {
         GameObject clone = Instantiate(vfxSpawn);
         clone.transform.position = gameObject.transform.position;
-
     }
     public void Death()
     {
@@ -23,13 +26,23 @@ public class EnemyBehavior : MonoBehaviour
     {
         // Vérifie si l'objet avec lequel il entre en collision est un ennemi
         if (other.CompareTag("Crystal"))
-        { 
-            Debug.Log("L'ennemi attaque le cristal!");
-            //Enemy enemy = other.GetComponent<Enemy>();
+        {
 
-            //enemy.NearTarget = true;
-           
+            Debug.Log("Crystal repéré");
+            if (animator != null)
+            {
+                animator.SetBool("NearTarget", true);
+            }
 
         }
     }
+    private void Start()
+    {
+        animator = GetComponent<Animator>();  // Récupère l'Animator attaché à l'ennemi
+        if (animator == null)
+        {
+            Debug.LogError("L'Animator de l'ennemi n'a pas été trouvé !");
+        }
+    }
+
 }
